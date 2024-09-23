13cb749b327c00f745a0486453f08515be0aa35a
abhishek-dhal <cse.22bcsh50@silicon.ac.in>
2024-09-23 09:09:08 -0400

    Edit left

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("portfoliomain/", include('portfoliomain.urls')),
+    path("", include('portfoliomain.urls')),
  
 
 ] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
